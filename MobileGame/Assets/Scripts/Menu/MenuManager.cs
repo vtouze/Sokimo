f0f7a4dec58c9b249e14e6e677348014f7cfa0d6
TeeZ <touzevirgile@gmail.com>
2025-07-22 15:11:25 +0200

    Menus Transition

--- MobileGame/Assets/Scripts/Menu/MenuManager.cs
+++ MobileGame/Assets/Scripts/Menu/MenuManager.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
-using UnityEngine.Video;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class MenuManager : MonoBehaviour
 {
     [Header("Menus")]
-    [SerializeField] private GameObject mainMenu;
-    [SerializeField] private GameObject settingsMenu;
-    [SerializeField] private GameObject shopMenu;
-    [SerializeField] private GameObject levelSelectionMenu;
+    [SerializeField] private RectTransform mainMenu;
+    [SerializeField] private RectTransform settingsMenu;
+    [SerializeField] private RectTransform shopMenu;
+    [SerializeField] private RectTransform levelSelectionMenu;
 
     [Header("Background Video")]
     [SerializeField] private VideoPlayer backgroundVideo;
 
-    private GameObject currentMenu;
+    private RectTransform currentMenu;
 
     private void Start()
     {
@@ -23,20 +23,75 @@ public class MenuManager : MonoBehaviour
             backgroundVideo.Play();
         }
 
-        ShowMenu(mainMenu);
+        HideAllMenus();
+        mainMenu.gameObject.SetActive(true);
+        currentMenu = mainMenu;
     }
 
-    public void ShowMenu(GameObject menuToShow)
+    private void HideAllMenus()
     {
-        if (currentMenu != null)
-            currentMenu.SetActive(false);
-
-        menuToShow.SetActive(true);
-        currentMenu = menuToShow;
+        mainMenu.gameObject.SetActive(false);
+        settingsMenu.gameObject.SetActive(false);
+        shopMenu.gameObject.SetActive(false);
+        levelSelectionMenu.gameObject.SetActive(false);
     }
 
-    public void ShowMainMenu() => ShowMenu(mainMenu);
-    public void ShowSettingsMenu() => ShowMenu(settingsMenu);
-    public void ShowShopMenu() => ShowMenu(shopMenu);
-    public void ShowLevelSelectionMenu() => ShowMenu(levelSelectionMenu);
+    public void ShowShopMenu()
+    {
+        ShowMenuFromDirection(shopMenu, Vector2.left);
+    }
+
+    public void ShowSettingsMenu()
+    {
+        ShowMenuFromDirection(settingsMenu, Vector2.right);
+    }
+
+    public void ShowLevelSelectionMenu()
+    {
+        ShowMenuFromDirection(levelSelectionMenu, Vector2.down);
+    }
+
+    public void BackToMainMenu()
+    {
+        if (currentMenu == null || currentMenu == mainMenu) return;
+
+        Vector2 exitDir = Vector2.zero;
+
+        if (currentMenu == shopMenu) exitDir = Vector2.left;
+        else if (currentMenu == settingsMenu) exitDir = Vector2.right;
+        else if (currentMenu == levelSelectionMenu) exitDir = Vector2.down;
+
+        RectTransform exiting = currentMenu;
+        currentMenu = mainMenu;
+
+        Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+        Vector3 exitPos = (Vector3)(exitDir * screenSize);
+
+        LeanTween.move(exiting, exiting.anchoredPosition3D + exitPos, 0.3f).setEase(LeanTweenType.easeInOutQuad).setOnComplete(() =>
+        {
+            exiting.gameObject.SetActive(false);
+            mainMenu.anchoredPosition3D = -exitPos;
+            mainMenu.gameObject.SetActive(true);
+            LeanTween.move(mainMenu, Vector3.zero, 0.3f).setEase(LeanTweenType.easeInOutQuad);
+        });
+    }
+
+    private void ShowMenuFromDirection(RectTransform targetMenu, Vector2 fromDirection)
+    {
+        if (currentMenu == targetMenu) return;
+
+        Vector2 screenSize = new Vector2(Screen.width, Screen.height);
+        Vector3 enterPos = (Vector3)(fromDirection * screenSize);
+
+        if (currentMenu != null)
+        {
+            currentMenu.gameObject.SetActive(false);
+        }
+
+        targetMenu.anchoredPosition3D = enterPos;
+        targetMenu.gameObject.SetActive(true);
+        LeanTween.move(targetMenu, Vector3.zero, 0.3f).setEase(LeanTweenType.easeInOutQuad);
+
+        currentMenu = targetMenu;
+    }
 }
